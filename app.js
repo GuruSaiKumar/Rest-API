5a5b6c90bbcc0724325d438ac07712299ae1584a
GuruSaiKumar <gurusaikumar2002@gmail.com>
2021-05-05 10:35:05 +0530

    Normal server with http

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('http')
+
+const server =http.createServer((req, res)=>{
+    if(req.url=='/'){
+        res.write("Hey there you are in root")
+        res.end();
+    }
+    if(req.url=='/customers'){
+        res.write("Yayy!! You have made it to customers section")
+        res.end();
+    }
+})
+
+server.listen(1234)
