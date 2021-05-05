415604efb8bcc1e27cb85991bdeecd60c12ed66d
GuruSaiKumar <gurusaikumar2002@gmail.com>
2021-05-05 13:39:29 +0530

    Learned to use id or name to find a specific course and 404 error

--- learning-express/index.js
+++ learning-express/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 
+const courses=[
+    {id: 1, name: 'Course1'},
+    {id: 2, name: 'Course2'},
+    {id: 3, name: 'Course3'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello world!!');
 });
 
+//all courses
 app.get('/api/courses', (req, res) => {
-    res.send(['course666', 'course2', 'course3']);
+    res.send(courses);
 });
 
-app.listen(3000, ()=> console.log('Listening on port 3000..'));
+//get specific course
+app.get('/api/courses/:id', (req, res) => {
+    const course= courses.find(c=>c.id===parseInt(req.params.id));
+    
+    //If not found the course 404 erroe
+    if(!course) res.status(404).send('The course with given id does not exits')
+    res.send(course);
+})
+//PORT
+// process.env.PORT =8080; 
+const port = process.env.PORT || 3000; // While hosting 3000 may not be available
+app.listen(port, ()=> console.log(`Listening on port ${port}..`));
